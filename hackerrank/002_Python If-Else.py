04e9770819a5a04764124f9ca274b3044612d571
Venci <pledgeshield@gmail.com>
2019-11-09 12:39:10 +0200

    Update 002_Python If-Else.py

--- hackerrank/002_Python If-Else.py
+++ hackerrank/002_Python If-Else.py
@@ -1,7 +1,19 @@
 
+# Name of problem: Python If-Else
+#
+# Created: Nov 9, 2019
+#
+# Revision #N/A / Date: N/A
+#
+# Link: 
+# https://www.hackerrank.com/challenges/py-if-else/problem
+
+
 '''
+Short description of the problem:
+
 if n is odd => Weird
-if n is even and in range 2 to 5 inclusive => Not Weird
+if n is even and in range 2 to  5 inclusive => Not Weird
 if n is even and in range 6 to 20 inclusive => Weird
 if n is even and n > 20 => Not Weird
 
@@ -11,7 +23,7 @@ CONSTRAINS: 1 <= n <= 100
 
 
 # All odd numbers are weird
-def weird(n):
+def weird_or_not(n):
 
     if n < 1 or n > 100:
         # print('Invalid input!')
@@ -22,8 +34,7 @@ def weird(n):
         print(f'{n:2} Not Weird')
 
 
+        
+#
 for x in range(31):
-    # weird(x)
-    pass
-
-# print(12 in range(6, 21))
+    weird_or_not(x)
